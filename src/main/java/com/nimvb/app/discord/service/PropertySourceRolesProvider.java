8045815259b35e51cd0a655e23a0478af400b1af
pdnco <pdnco@protonmail.com>
2022-02-16 14:52:14 +0330

    feat(roles): Add an implementation for the default roles providing convention mentioned at 7ab6df
    Refs #3

--- src/main/java/com/nimvb/app/discord/service/PropertySourceRolesProvider.java
+++ src/main/java/com/nimvb/app/discord/service/PropertySourceRolesProvider.java
@@ -1,2 +1,21 @@
-package com.nimvb.app.discord.service;public class PropertySourceRolesProvider {
+package com.nimvb.app.discord.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class PropertySourceRolesProvider implements RolesProvider{
+
+
+    @Value("${spring.security.roles.default:#{T(com.nimvb.app.discord.service.RolesProvider).DEFAULT_ROLES}}")
+    String[] roles;
+
+    @Override
+    public Set<Role> provide() {
+        return Collections.unmodifiableSet(Arrays.stream(roles).map(Role::deserialize).collect(Collectors.toUnmodifiableSet()));
+    }
 }
