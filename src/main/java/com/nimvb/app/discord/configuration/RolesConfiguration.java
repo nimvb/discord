57e6fbfeb8370a3adf782f8c3e9f1e4b4b644b4c
pdnco <pdnco@protonmail.com>
2022-02-16 15:09:37 +0330

    feat(roles): Add the configuration to provide the default bean for the  implementation of the default-roles-provider convention 804581 7ab6df
    Refs #3
    
    test(roles): Add tests for the added configuration
    
    Refs #3

--- src/main/java/com/nimvb/app/discord/configuration/RolesConfiguration.java
+++ src/main/java/com/nimvb/app/discord/configuration/RolesConfiguration.java
@@ -1,2 +1,15 @@
-package com.nimvb.app.discord.configuration;public class RolesConfiguration {
+package com.nimvb.app.discord.configuration;
+
+import com.nimvb.app.discord.service.PropertySourceRolesProvider;
+import com.nimvb.app.discord.service.RolesProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RolesConfiguration {
+
+    @Bean
+    RolesProvider rolesProvider(){
+        return new PropertySourceRolesProvider();
+    }
 }
--- src/test/java/com/nimvb/app/discord/configuration/RolesConfigurationTest.java
+++ src/test/java/com/nimvb/app/discord/configuration/RolesConfigurationTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.nimvb.app.discord.configuration;
+
+import com.nimvb.app.discord.service.RolesProvider;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 class RolesConfigurationTest {
-  
+
+    @Test
+    void ShouldProvideABeanForRolesProvider(){
+        new ApplicationContextRunner()
+                .withUserConfiguration(RolesConfiguration.class)
+                .run(context -> {
+                    Assertions.assertThat(context).hasSingleBean(RolesProvider.class);
+                });
+    }
+
 }
